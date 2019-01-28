30078b58afe629e50ead2e48b94559792ad2637f
ZoliEsq <40950818+ZoliEsq@users.noreply.github.com>
2019-01-28 15:29:36 -0500

    robofriends

---
+++ Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+  	return (
+	  	<div>
+	  		<h1>Hello World</h1>
+	  		<p>Welcome To React</p>
+	  	</div>
+  	);
+  }
+}
+
+export default Hello;
